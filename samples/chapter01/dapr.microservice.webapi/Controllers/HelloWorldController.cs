87f00ecfe60be3431f1ad23b1f7406609e0a4ad2
Mohammed O Rahman <mobaidr@gmail.com>
2021-10-08 01:31:39 +0530

    First Sample project

---
+++ samples/chapter01/dapr.microservice.webapi/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+using Dapr;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace dapr.microservice.webapi.Controllers
+{
+
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+
+        [HttpGet("hello")]
+        public ActionResult<string> Get()
+        {
+            Console.WriteLine("Hello World");
+
+            return "Hello World";
+        }
+    }
+}
